1dea64f0aa8d1420c4f0b57739e2f0e232cf008d
himanshu <himanshu53939@gmail.com>
2016-03-30 11:34:05 -0700

    gg

--- www/js/index.js
+++ www/js/index.js
@@ -120,7 +120,7 @@ var app={
         $('#userLogo').html(app.creteHtml('userProfileTemplate',data));
     },
     loadProfile2:function(){
-        var response={"status":1,"data":{"s_id":"13","s_email":"hhh@gmail.com","s_name":"hhh","s_zip":"9999","s_contact":" 789879855","s_password":"$2y$13$BcLqRwSAsIy0gvjOafvKBO3j82yYepHiubk4R7GOeDOgZirKfdZqq","s_auth_key":"","s_password_reset_token":"","s_logo":"1458678099.jpg","s_address":"fgdfhgfh","s_lat":"","s_long":"","s_created":"1458267296","s_modified":"1458678726","img":"http://localhost/gava/web/upload/profile/1458678099.jpg"}};
+        var response={"status":1,"data":{"s_id":"13","s_email":"hhh@gmail.com","s_name":"hhh","s_zip":"9999","s_contact":" 789879855","s_password":"$2y$13$BcLqRwSAsIy0gvjOafvKBO3j82yYepHiubk4R7GOeDOgZirKfdZqq","s_auth_key":"","s_password_reset_token":"","s_logo":"profile_pic_03.png","s_address":"fgdfhgfh","s_lat":"","s_long":"","s_created":"1458267296","s_modified":"1458678726","img":"img/profile_pic_03.png"}};
         app.stopLoader();
         if(response.message){app.alert(response.message);}
         if(response.status) {
