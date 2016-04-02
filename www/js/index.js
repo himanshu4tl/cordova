47194f15686be9539555e5696b772d2d04bad7f2
himanshu <himanshu53939@gmail.com>
2016-04-01 18:53:50 -0700

    hhh

--- www/js/index.js
+++ www/js/index.js
@@ -181,12 +181,39 @@ var app={
        this.setTitle('PROFILE<div class="closeBtnTitle"><img class="inlineblock likeBtnTitle" src="img/close.png"><img class="inlineblock likeBtnTitle" src="img/like.png"><div>')
     },
     homeInit:function(){
-        $(".owl-carousel").owlCarousel({
+        /*$(".owl-carousel").owlCarousel({
             items:1,
             loop:false,
             nav:false,
+        });*/
+        /**
+         * jTinder initialization
+         */
+        $("#tinderslide").jTinder({
+            // dislike callback
+            onDislike: function (item) {
+                // set the status text
+                $('#status').html('Dislike image ' + (item.index()+1));
+            },
+            // like callback
+            onLike: function (item) {
+                // set the status text
+                $('#status').html('Like image ' + (item.index()+1));
+            },
+            animationRevertSpeed: 200,
+            animationSpeed: 400,
+            threshold: 1,
+            likeSelector: '.like',
+            dislikeSelector: '.dislike'
         });
 
+        /**
+         * Set button action to trigger jTinder like & dislike.
+         */
+        $('.actions .like, .actions .dislike').click(function(e){
+            e.preventDefault();
+            $("#tinderslide").jTinder($(this).attr('class'));
+        });
     },
     restaurentInit:function(){
         $(".owl-carousel").owlCarousel({
